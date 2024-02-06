6f5e8e1efd29b5ee69ba5dc122789bd8ec795a6c
correalm <luismigueldasilva.99@gmail.com>
2024-02-06 13:49:43 -0300

    chore: adjust types and user validation

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -1,27 +1,31 @@
 import { UserErrors } from "../errors";
 import { UserModel } from "../models/user";
 import bcrypt from "bcrypt";
-import { Express, RequestHandler, Response } from "express";
+import { Express, Response } from "express";
 
 export interface TypedRequestBody<T> extends Express.Request {
   body: T
 }
 
-async function registerUser(req: TypedRequestBody<{ username: string, password: string, isAdmin: boolean }>, res: Response): Promise<RequestHandler> {
+async function registerUser(
+  req: TypedRequestBody<{ username: string, password: string, isAdmin: boolean }>,
+  res: Response
+): Promise<void> {
   const { username, password, isAdmin = false } = req.body;
 
   try {
     const user = await UserModel.findOne({ username });
 
     if (user) {
-      return res.status(400).json({ type: UserErrors.USERNAME_ALREADY_EXISTS });
+      res.status(400).json({ type: UserErrors.USERNAME_ALREADY_EXISTS });
+      return;
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new UserModel({ username, password: hashedPassword, isAdmin  });
     await newUser.save();
 
-    res.json({ message: "User registered successfully" });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
     res.status(500).json({ type: err });
   }
