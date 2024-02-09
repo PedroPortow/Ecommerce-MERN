c28f5e470327f477df1f0cbfe9ad75ef6ed8f9db
PedroPortow <pedrolportow@gmail.com>
2024-02-08 22:10:45 -0300

    fix: fixed missing key inside loop

--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -10,9 +10,8 @@ const Shop: React.FC = () => {
     <div className="p-4 bg-slate-100 min-h-screen">
       <div className="flex flex-wrap m04 justify-center gap-y-4"> 
         {products.map((product: IProduct) => (
-          <div className="p-2 sm:w-1/2 md:w-1/3 lg:w-1/4"> 
+          <div key={product._id} className="p-2 sm:w-1/2 md:w-1/3 lg:w-1/4"> 
             <ProductCard
-              key={product._id}
               _id={product._id}
               name={product.name}
               price={product.price}
